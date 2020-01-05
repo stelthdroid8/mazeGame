5dc62ce60f5eb42a56274786eb4b72eea6f6d498
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-05 16:49:43 -0500

    add starting logic to find the neighbors of a randomized spot in the maze

--- index.js
+++ index.js
@@ -36,12 +36,26 @@ World.add(world, walls);
 
 
 // maze generation
+
+const shuffleNeighbors = (arr) => {
+    let counter = arr.length;
+
+    while( counter > 0){
+        const index = Math.floor(Math.random() * counter);
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+    return arr;
+}
 const grid = 
 Array(cells)
     .fill(null)
     .map(() => Array(cells)
     .fill(false));
-    
+
 // console.log(grid);
 
 const verticals = 
@@ -60,3 +74,37 @@ const horizontals =
 
 // console.log(verticals);
 // console.log(horizontals);
+
+const startRow  = Math.floor(Math.random() * cells);
+const startCol  = Math.floor(Math.random() * cells);
+
+const gridWallSetup = (row, col) =>{
+    // if cell[row, col] visited, return
+    if(grid[row][col]){
+        return;
+    }
+    // else mark visited => grid array update
+    grid[row][col] = true;
+
+    // Assemble random order of neighbor
+    const neighbors = shuffleNeighbors([
+        //top
+        [row-1, col],
+        //right 
+        [row, col +1],
+        //bottom
+        [row +1, col],
+        //left
+        [row, col -1]
+    ]);
+
+    // For each neighbor
+        // check neighbor bounds
+            // if visited continue to next
+            // else visit next cell
+            // remove the wall from the corresponding tracking array
+            
+}
+
+gridWallSetup(1, 1);
+
